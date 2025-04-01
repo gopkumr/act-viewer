1c78d4afdd048bace706276cf2e493ef9ed3191f
gopa.ravi <gopa.ravi@arinco.com.au>
2025-04-02 00:04:55 +1100

    Added deployment

---
+++ deploy/deploy.ps1
@@ -0,0 +1,7 @@
+
+Set-Location ..
+
+# Login to Azure
+azd auth login --scope https://management.azure.com//.default
+
+azd up
--- src/Host/AppHost/Program.cs
+++ src/Host/AppHost/Program.cs
@@ -2,9 +2,5 @@ using Aspire.Hosting;
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
-       
-
 builder.AddProject<Projects.ACRViewer_BlazorServer>("acrviewer-blazorserver");
-       
-
 builder.Build().Run();
