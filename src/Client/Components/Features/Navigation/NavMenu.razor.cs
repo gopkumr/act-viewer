a1274b2527914c1719dd9d3497fce47441dd76a0
gopa.ravi <gopa.ravi@arinco.com.au>
2025-05-10 22:06:52 +1000

    Fixed search

--- src/Client/Components/Features/Navigation/NavMenu.razor.cs
+++ src/Client/Components/Features/Navigation/NavMenu.razor.cs
@@ -9,7 +9,7 @@ namespace Arinco.BicepHub.App.Components.Features.Navigation
     public partial class NavMenu
     {
         private List<TreeItemData<TreeViewItemViewModel>> Repositories { get; set; } = [];
-        [Inject] private INavigationService? NavigationService{ get; set; }
+        [Inject] private INavigationService? NavigationService { get; set; }
 
         [Inject] private NavigationManager NavigationManager { get; set; }
 
@@ -108,14 +108,13 @@ namespace Arinco.BicepHub.App.Components.Features.Navigation
         }
         private Task<bool> MatchesName(TreeItemData<TreeViewItemViewModel> item)
         {
-            if (item?.Value?.Type == TreeViewType.Tag) return Task.FromResult(true);
+            if (string.IsNullOrEmpty(_searchPhrase))
+                return Task.FromResult(true);
 
-            if (string.IsNullOrEmpty(item?.Value?.Name))
-            {
-                return Task.FromResult(false);
-            }
+            if (item?.Value?.Type == TreeViewType.Tag)
+                return Task.FromResult((item?.Value?.ParentName ?? "").Contains(_searchPhrase, StringComparison.OrdinalIgnoreCase));
 
-            return Task.FromResult(item.Value.Name.Contains(_searchPhrase, StringComparison.OrdinalIgnoreCase));
+            return Task.FromResult((item?.Value?.Name ?? "").Contains(_searchPhrase, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
